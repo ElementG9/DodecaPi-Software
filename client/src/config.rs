320d90015dd1dd71bc151a672c9490fe0016b132
ElementG9 <garentyler@gmail.com>
2020-03-11 09:25:45 -0600

    Create client

---
+++ client/src/config.rs
@@ -0,0 +1,23 @@
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub portin: u16
+}
+
+pub fn read_config(filename: &str) -> io::Result<Config> {
+    let data = read_file(filename)?;
+    let conf: Config = serde_json::from_str(&data)?;
+    Ok(Config {
+        portin: conf.portin
+    })
+}
+fn read_file(filename: &str) -> io::Result<String> {
+    let mut f = File::open(filename)?;
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer)?;
+    Ok(buffer)
+}
---
+++ client/src/lib.rs
@@ -0,0 +1,13 @@
+pub mod config;
+pub mod network;
+
+pub fn start(config_filename: &str) {
+    let conf = config::read_config(config_filename).unwrap();
+    let listener = network::start_listener(conf.portin)
+        .expect(&format!("Could not start listener on port {}", conf.portin));
+    println!("Started listener on port {}", conf.portin);
+    for stream in listener.incoming() {
+        let mut stream = stream.expect("Could not open stream");
+        network::handle_stream(&mut stream, &conf);
+    }
+}
