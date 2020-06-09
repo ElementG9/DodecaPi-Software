450907b20a6cd8d8c81e5cac6350feca4be56285
Danton <43303409+khimno@users.noreply.github.com>
2020-06-08 22:08:17 -0700

    Basic child application framework

--- child/src/main.rs
+++ child/src/main.rs
@@ -1,3 +1,25 @@
+extern crate protocol;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("What port should the application listen on?");
+    let mut port = String::new();
+    io::stdin()
+        .read_line(&mut port)
+        .expect("Failed to get port");
+
+    println!("Master information in IP:Port")
+    let mut master = String::new();
+    io::stdin()
+        .read_line(&mut master)
+        .expect("Failed to read");
+
+    let listener = TcpListener::bind("0.0.0.0:{}", port);
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+
+    }
 }
