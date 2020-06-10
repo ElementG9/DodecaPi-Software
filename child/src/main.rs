95ef13c371763caca980b3cde4e9928942d94fec
Danton <43303409+khimno@users.noreply.github.com>
2020-06-10 14:36:33 -0700

    Working listener and sender

--- child/src/main.rs
+++ child/src/main.rs
@@ -1,25 +1,36 @@
 extern crate protocol;
-use std::net::TcpStream;
 use std::net::TcpListener;
 use std::io;
+use std::io::prelude::*;
 
 fn main() {
-    println!("Which interface should the application listen on? (Example: 0.0.0.0:420 for all interfaces, port 420)");
-    let mut listen = String::new();
-    io::stdin()
-        .read_line(&mut listen)
-        .expect("Failed to read");
-
-    println!("Master information in IP:Port");
+    println!("Master location (Example: 192.168.35.50:6500)");
     let mut master = String::new();
     io::stdin()
         .read_line(&mut master)
         .expect("Failed to read");
 
-    let listener = TcpListener::bind("0.0.0.0:6500").unwrap();
+    let mut listener = TcpListener::bind("0.0.0.0:6500").unwrap();
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
+        println!("got something");
+        let mut packetBuffer = Vec::new();
+        stream.read_to_end(&mut packetBuffer);
+        println!("Hex: {:X?}", packetBuffer);
+
+        let length = protocol::Packet::read_u32(0, &packetBuffer);
+        let id = protocol::Packet::read_u8(4, &packetBuffer);
+        let s = protocol::Packet::read_string(5, &packetBuffer);
+        println!("Length: {}, ID: {}, String: \"{}\"", length, id, s);
+
+        if id == 9 {
+            let mut p = protocol::Packet::new(0x00);
+            p.write_string("test".to_owned());
+
+            let bytes = p.get_bytes();
+            stream.write(&bytes);
+        }
 
     }
 }
--- parent/src/main.rs
+++ parent/src/main.rs
@@ -1,4 +1,5 @@
 extern crate protocol;
+use std::io::prelude::*;
 use std::net::TcpStream;
 
 fn main() {
@@ -12,4 +13,8 @@ fn main() {
     let id = protocol::Packet::read_u8(4, &bytes);
     let s = protocol::Packet::read_string(5, &bytes);
     println!("Length: {}, ID: {}, String: \"{}\"", length, id, s);
+
+    let mut stream = TcpStream::connect("127.0.0.1:6500").unwrap();
+
+    stream.write(&bytes);
 }
