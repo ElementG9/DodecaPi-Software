c305c5d6079c5dff574d14b5b409c0f00f1ead1c
ElementG9 <garentyler@gmail.com>
2020-06-10 17:46:09 -0600

    Rewrite child

--- child/src/main.rs
+++ child/src/main.rs
@@ -1,36 +1,24 @@
 extern crate protocol;
-use std::net::TcpListener;
-use std::io;
-use std::io::prelude::*;
+use protocol::packet::*;
+use std::net::{TcpListener, TcpStream};
+
+static SERVER_PORT: u16 = 6500;
+static SERVER_PROTOCOL_VERSION: u8 = 2;
 
 fn main() {
-    println!("Master location (Example: 192.168.35.50:6500)");
-    let mut master = String::new();
-    io::stdin()
-        .read_line(&mut master)
-        .expect("Failed to read");
-
-    let mut listener = TcpListener::bind("0.0.0.0:6500").unwrap();
-
+    let listener_address = &format!("0.0.0.0:{}", SERVER_PORT);
+    let listener = TcpListener::bind(listener_address).expect("could not create listener");
+    println!("Started listener at {}", listener_address);
     for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-        println!("got something");
-        let mut packetBuffer = Vec::new();
-        stream.read_to_end(&mut packetBuffer);
-        println!("Hex: {:X?}", packetBuffer);
-
-        let length = protocol::Packet::read_u32(0, &packetBuffer);
-        let id = protocol::Packet::read_u8(4, &packetBuffer);
-        let s = protocol::Packet::read_string(5, &packetBuffer);
-        println!("Length: {}, ID: {}, String: \"{}\"", length, id, s);
-
-        if id == 9 {
-            let mut p = protocol::Packet::new(0x00);
-            p.write_string("test".to_owned());
-
-            let bytes = p.get_bytes();
-            stream.write(&bytes);
-        }
-
+        let _ = handle_connection(stream.expect("could not make connection"));
     }
+}
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+    println!("Got a connection");
+    // Read 0x00 Handshake
+    let protocol_version = Handshake::read(&mut stream)?.protocol_version;
+    println!("Client wants protocol version {}", protocol_version);
+    // Reply with 0x08 Handshake Response
+    let _ = HandshakeResponse::new().write(&mut stream)?;
+    Ok(())
 }
