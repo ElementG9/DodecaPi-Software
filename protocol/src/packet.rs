04e79207347939221e07559d0171230639626a5b
ElementG9 <garentyler@gmail.com>
2020-06-10 17:46:02 -0600

    Rename read_header to read_packet_header

--- protocol/src/packet.rs
+++ protocol/src/packet.rs
@@ -1,7 +1,7 @@
 use crate::helper::*;
 use std::net::TcpStream;
 
-pub fn read_header(t: &mut TcpStream) -> std::io::Result<(u32, u8)> {
+pub fn read_packet_header(t: &mut TcpStream) -> std::io::Result<(u32, u8)> {
     let packet_length = read_u32(t)? - 1;
     let packet_id = read_u8(t)?;
     Ok((packet_length, packet_id))
@@ -9,16 +9,16 @@ pub fn read_header(t: &mut TcpStream) -> std::io::Result<(u32, u8)> {
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Handshake {
-    protocol_number: u8,
+    pub protocol_version: u8,
 }
 impl Handshake {
-    pub fn new(protocol_number: u8) -> Handshake {
-        Handshake { protocol_number }
+    pub fn new(protocol_version: u8) -> Handshake {
+        Handshake { protocol_version }
     }
     pub fn read(t: &mut TcpStream) -> std::io::Result<Handshake> {
-        let (_len, _id) = read_header(t)?;
-        let protocol_number = read_u8(t)?;
-        Ok(Handshake::new(protocol_number))
+        let (_len, _id) = read_packet_header(t)?;
+        let protocol_version = read_u8(t)?;
+        Ok(Handshake::new(protocol_version))
     }
     pub fn write(&self, t: &mut TcpStream) -> std::io::Result<()> {
         write_bytes(t, &self.to_bytes())?;
@@ -28,7 +28,7 @@ impl Handshake {
         let mut bytes = Vec::new();
         write_vec_u32(&mut bytes, 2); // 2: 1 for ID, 1 for protocol_number
         write_vec_u8(&mut bytes, 0x00); // 0x00 Handshake
-        write_vec_u8(&mut bytes, self.protocol_number);
+        write_vec_u8(&mut bytes, self.protocol_version);
         bytes
     }
 }
@@ -40,7 +40,7 @@ impl HandshakeResponse {
         HandshakeResponse {}
     }
     pub fn read(t: &mut TcpStream) -> std::io::Result<HandshakeResponse> {
-        let (_len, _id) = read_header(t)?;
+        let (_len, _id) = read_packet_header(t)?;
         Ok(HandshakeResponse::new())
     }
     pub fn write(&self, t: &mut TcpStream) -> std::io::Result<()> {
@@ -62,7 +62,7 @@ impl Ping {
         Ping {}
     }
     pub fn read(t: &mut TcpStream) -> std::io::Result<Ping> {
-        let (_len, _id) = read_header(t)?;
+        let (_len, _id) = read_packet_header(t)?;
         Ok(Ping::new())
     }
     pub fn write(&self, t: &mut TcpStream) -> std::io::Result<()> {
@@ -84,7 +84,7 @@ impl Pong {
         Pong {}
     }
     pub fn read(t: &mut TcpStream) -> std::io::Result<Pong> {
-        let (_len, _id) = read_header(t)?;
+        let (_len, _id) = read_packet_header(t)?;
         Ok(Pong::new())
     }
     pub fn write(&self, t: &mut TcpStream) -> std::io::Result<()> {
