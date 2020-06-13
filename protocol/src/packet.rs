9d40e35aa3abc59eb6401aff53b7a16f8bfb12d7
Danton <43303409+khimno@users.noreply.github.com>
2020-06-12 18:09:45 -0700

    Integrate changes to protocol

--- protocol/src/packet.rs
+++ protocol/src/packet.rs
@@ -122,26 +122,24 @@ impl Disconnect {
     }
 }
 
-// Currently all u64s for the factor ranges for simplicity
+
+// neko miko reimu ai shiteru neko miko reimu nani shiteru neko miko reimu shuran yurari
 #[derive(Debug, Clone, PartialEq)]
 pub struct FactorRequest {
-    pub num_type: u8,
     pub range_start: u64,
     pub range_end: u64,
 }
 impl FactorRequest {
-    pub fn new(num_type: u8, range_start: u64, range_end: u64) -> FactorRequest {
+    pub fn new(range_start: u64, range_end: u64) -> FactorRequest {
         FactorRequest {
-            num_type,
             range_start,
             range_end,
         }
     }
     pub fn read(t: &mut TcpStream) -> std::io::Result<FactorRequest> {
-        let num_type = read_u8(t)?;
         let range_start = read_u64(t)?;
         let range_end = read_u64(t)?;
-        Ok(FactorRequest::new(num_type, range_start, range_end))
+        Ok(FactorRequest::new(range_start, range_end))
     }
     pub fn write(&self, t: &mut TcpStream) -> std::io::Result<()> {
         write_bytes(t, &self.to_bytes())?;
@@ -149,7 +147,7 @@ impl FactorRequest {
     }
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
-        write_vec_u32(&mut bytes, 17); // 17: 1 for ID, 8 for range_start, 8 for range_end
+        write_vec_u32(&mut bytes, 16); // 16: 8 for range_start, 8 for range_end
         write_vec_u8(&mut bytes, 0x05); // 0x05 FactorRequest
         write_vec_u64(&mut bytes, self.range_start);
         write_vec_u64(&mut bytes, self.range_end);
@@ -157,24 +155,21 @@ impl FactorRequest {
     }
 }
 
-// Currently all u64s for the factor ranges for simplicity
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct FactorResponse {
     pub found_factor: bool,
-    pub factor_value_type: u8,
     pub factor_value: u64,
 }
 impl FactorResponse {
-    pub fn new(found_factor: bool, factor_value_type: u8, factor_value: u64) -> FactorResponse {
+    pub fn new(found_factor: bool, factor_value: u64) -> FactorResponse {
         FactorResponse {
             found_factor,
-            factor_value_type,
             factor_value,
         }
     }
     pub fn read(t: &mut TcpStream) -> std::io::Result<FactorResponse> {
         let found_factor = read_u8(t)? == 0x01;
-        let factor_value_type = read_u8(t)?;
         let factor_value = read_u64(t)?;
         Ok(FactorResponse::new(
             found_factor,
@@ -188,7 +183,7 @@ impl FactorResponse {
     }
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
-        write_vec_u32(&mut bytes, 11); // 11: 1 for ID, 1 for found_factor, 1 for factor_value_type, 8 for factor_value
+        write_vec_u32(&mut bytes, 9); // ⑨: 1 for found_factor 8 for factor_value
         write_vec_u8(&mut bytes, 0x06); // 0x06 FactorResponse
         write_vec_u8(
             &mut bytes,
@@ -197,7 +192,6 @@ impl FactorResponse {
                 false => 0x00,
             },
         );
-        write_vec_u8(&mut bytes, self.factor_value_type);
         write_vec_u64(&mut bytes, self.factor_value);
         bytes
     }
